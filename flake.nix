5c394d316b1c035cbb646a7479fcf04c8440b0c4
Mohammad Al-Ahdal <mo@mohammad.dev>
2025-06-14 13:36:23 -0600

    update flake.lock and flake.nix with new dependencies and remove unused nixConfig settings

--- flake.nix
+++ flake.nix
@@ -72,10 +72,4 @@
           inputsFrom = builtins.attrValues self.checks.${system};
         };
       });
-
-  nixConfig = {
-    # Cache for the Rust toolchain in fenix
-    extra-substituters = [ "https://nix-community.cachix.org" ];
-    extra-trusted-public-keys = [ "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs=" ];
-  };
 }
